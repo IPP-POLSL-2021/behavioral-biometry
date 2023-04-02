e9f65ec676bfc840bb12423de0d2175dca067927
kriskros <krzysztofczuba884@gmail.com>
2023-04-02 16:48:51 +0200

    locking fix

--- frontend/src/WordState/index.tsx
+++ frontend/src/WordState/index.tsx
@@ -38,7 +38,11 @@ const WordState = ({ word } : { word: string }) => {
 
   const handleKeyDown = (event: KeyboardEvent) => {
     setLetters((current) => {
-      const currentIndex = current.findIndex(letter => event.key === letter.symbol && !letter.downTimestamp);
+      const currentIndex = current.findIndex((letter, idx) => 
+        getLastIndex(current, (letter) => Boolean(letter.downTimestamp)) === idx - 1 
+        && event.key === letter.symbol 
+        && !letter.downTimestamp
+      );
       if (currentIndex == -1) {
         return current;
       }
