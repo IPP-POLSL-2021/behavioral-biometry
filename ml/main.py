12bd7f10b92e10bbc799a9983425982b9177ff94
H4wk <quasand89@gmail.com>
2023-04-13 19:29:31 +0200

    type hinting

--- ml/main.py
+++ ml/main.py
@@ -1,20 +1,28 @@
-import pandas as pd
-import numpy as np
-from utils import normalize, split
-from knn import KNN
 from time import time
-from sklearn.neighbors import KNeighborsClassifier
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+from knn import KNN
+from utils import normalize, split
 
 if __name__ == "__main__":
-    df = pd.read_csv('data.csv')
-    df.drop(columns=['sessionIndex','rep'], axis=1, inplace=True)
+    df = pd.read_csv("data.csv")
+    df.drop(columns=["sessionIndex", "rep"], axis=1, inplace=True)
     values = df.columns.values[1:]
 
     df = df.sample(frac=1)
-    X = np.array([np.array(normalize(df.loc[:, value])) for value in values])
+    X = np.array(
+        [
+            np.array(normalize(df.loc[:, value]), dtype=np.float64)
+            for value in values
+        ],
+        dtype=np.float64,
+    )
     X = X.T
-    Y = np.array(df.loc[:, "subject"])
+    Y = np.array(df.loc[:, "subject"], dtype=np.string_)
 
     x_train, x_test = split(X, 0.8)
     y_train, y_test = split(Y, 0.8)
@@ -27,14 +35,16 @@ if __name__ == "__main__":
         model.fit(x_train, y_train)
         predicted = model.predict(x_test)
         accuracy = model.score(x_test, y_test)
-        # accuracy = sum([predicted[i] == y_test[i] for i in range(len(y_test))]) / len(y_test)
+        # accuracy = sum(
+        #     [predicted[i] == y_test[i] for i in range(len(y_test))]
+        # ) / len(y_test)
         print("time taken (seconds): " + str(time() - start))
         print(f"accuracy: {round(accuracy, 3)}")
         for i in range(len(predicted)):
             f.write(f"Predicted: {predicted[i]}, actual: {y_test[i]}\n")
 
     # plt.figure(figsize=(10,6))
-    # plt.plot(range(1,142+1, 2),acc,color = 'blue',linestyle='dashed', 
+    # plt.plot(range(1,142+1, 2),acc,color = 'blue',linestyle='dashed',
     #          marker='o',markerfacecolor='red', markersize=10)
     # plt.title('accuracy vs. K Value')
     # plt.xlabel('K')
@@ -42,6 +52,5 @@ if __name__ == "__main__":
     # print("Maximum accuracy:-",max(acc),"at K =",acc.index(max(acc)) + 1)
     # plt.savefig("acc_k_correlation.png")
     # plt.show()
-
 
     # time taken for scikit learn knn: 0.66
--- ml/utils.py
+++ ml/utils.py
@@ -1,13 +1,19 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-def normalize(dat: pd.Series) -> pd.Series:
+
+def normalize(dat: "pd.Series[float]") -> "pd.Series[float]":
     """Normalize list elements to range [0, 1]."""
     mn = dat.min()
     mx = dat.max()
     return (dat - mn) / (mx - mn)
 
-def split(dat: np.ndarray, train_size: float) -> tuple[np.ndarray, np.ndarray]:
+
+def split(
+    dat: np.ndarray[np.ndarray[np.float64]], train_size: np.float64
+) -> tuple[
+    np.ndarray[np.ndarray[np.float64]], np.ndarray[np.ndarray[np.float64]]
+]:
     """Split list into 2 smaller lists given by fraction."""
     idx = int(len(dat) * train_size)
     return dat[:idx], dat[idx:]
