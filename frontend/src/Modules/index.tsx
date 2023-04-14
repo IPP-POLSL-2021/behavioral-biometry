8c7d6ba596925e2fa54a058a3865170722c0ef22
H4wk <quasand89@gmail.com>
2023-04-14 17:09:15 +0200

    fix logged as message not showing after refresh

--- frontend/src/Modules/index.tsx
+++ frontend/src/Modules/index.tsx
@@ -10,19 +10,32 @@ import { LoggedUsernameContext } from "../LoggedUsernameContext"
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useCookies } from 'react-cookie';
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 
 const Layout = ({children}: {children: JSX.Element}) => {
-  const updateUsername = (username: string) => setLoggedUsername({username: username, updateUsername: updateUsername});
-  const [loggedUsername, setLoggedUsername] = useState({username: "", updateUsername: updateUsername});
+  const loggedUsername = useContext(LoggedUsernameContext)
+  const [cookies, ] = useCookies(["access_token"]);
+
+  useEffect(() => {
+    if (cookies.access_token) {
+      fetch("http://localhost:5050/api/users/current", {
+        headers: { 
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: cookies.access_token
+        }
+      })
+      .then((res) => res.ok ? res.json() : Promise.reject(res))
+      .then((data) => loggedUsername.updateUsername(data.userName))
+      .catch((err) => console.error(err))
+    }
+  }, [])
 
   return (
     <>
-    <LoggedUsernameContext.Provider value={loggedUsername}>
       <Header />
       {children}
-    </LoggedUsernameContext.Provider>
     </>
   )
 }
@@ -80,7 +93,14 @@ const router = createBrowserRouter([
 ]);
 
 const ModuleRouter = () => {
-  return <RouterProvider router={router} />
+  const updateUsername = (username: string) => setLoggedUsername({username: username, updateUsername: updateUsername});
+  const [loggedUsername, setLoggedUsername] = useState({username: "", updateUsername: updateUsername});
+
+  return (
+    <LoggedUsernameContext.Provider value={loggedUsername}>
+      <RouterProvider router={router} />
+    </LoggedUsernameContext.Provider>
+  );
 }
 
 // TODO: WordStateContoller zależny od stanu logowania użytkownika
