c8515ccc4102b393424e67c46fe260476094a0fd
kingbrucelee <90533878+kingbrucelee@users.noreply.github.com>
2023-05-04 19:54:56 +0200

    Logistic Regression
    and fixed low precision and slight wording changes

--- ml/test_classifiers.py
+++ ml/test_classifiers.py
@@ -8,6 +8,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 
 from utils import load_keystroke, normalize
 
@@ -22,7 +23,7 @@ def test(data, classifier):
     y_pred = clf.predict(X_test)
     total_time = time() - start
     accuracy = accuracy_score(y_test, y_pred)
-    return round(accuracy, 2), round(total_time, 2)
+    return round(accuracy, 2), round(total_time, 5)
 
 
 names = [
@@ -34,6 +35,7 @@ names = [
     "Neural Net",
     "AdaBoost",
     "Naive Bayes",
+    "Logistic Regression",
 ]
 
 classifiers = [
@@ -45,14 +47,13 @@ classifiers = [
     MLPClassifier(alpha=1, max_iter=1000),
     AdaBoostClassifier(),
     GaussianNB(),
+    LogisticRegression(max_iter=1000),
 ]
 
 for name, classifier in zip(names, classifiers):
     print(f"\n{name}:\n")
-    print(f"Accuracy Iris {test(datasets.load_iris(), classifier)}")
-    print(f"Accuracy Wine {test(datasets.load_wine(), classifier)}")
-    print(
-        "Accuracy Breast Cancer" f" {test(datasets.load_breast_cancer(), classifier)}"
-    )
-    print(f"Accuracy Digits {test(datasets.load_digits(), classifier)}")
-    print(f"Accuracy KeystrokeDynamics {test(load_keystroke(), classifier)}")
+    print(f"Iris {test(datasets.load_iris(), classifier)}")
+    print(f"Wine {test(datasets.load_wine(), classifier)}")
+    print(f"Breast Cancer {test(datasets.load_breast_cancer(), classifier)}")
+    print(f"Digits {test(datasets.load_digits(), classifier)}")
+    print(f"KeyStrokeDynamics {test(load_keystroke(), classifier)}")
