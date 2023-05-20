1356d57ec37332fa1ae265ee423a007bd71f59b6
H4wk <quasand89@gmail.com>
2023-05-20 04:34:24 +0200

    add auth with python api

--- frontend/src/Modules/WordState/index.tsx
+++ frontend/src/Modules/WordState/index.tsx
@@ -1,85 +1,98 @@
-import { useEffect, useState } from 'react';
-import OnlineController from '../../Components/WordState/OnlineController'
-import styles from './style.module.scss'
-import { convertPayload } from '../../Components/WordState/helpers';
-import { Itoken } from '../../Components/WordState/interfaces';
+import { useEffect, useState } from "react";
+import OnlineController from "../../Components/WordState/OnlineController";
+import styles from "./style.module.scss";
+import { convertPayload } from "../../Components/WordState/helpers";
+import { Itoken } from "../../Components/WordState/interfaces";
 
-type promptType = 'fixed' | 'flex' | 'done';
+type promptType = "fixed" | "flex" | "done";
 
-const Auth = ({userId}: {userId: number}) => {
-  const [ promptType, setPromptType ] = useState<promptType>('fixed');
+const Auth = ({ userId }: { userId: number }) => {
+  const [promptType, setPromptType] = useState<promptType>("fixed");
+  const [fixedAuth, setFixedAuth] = useState(false);
+  const [flexAuth, setFlexAuth] = useState(false);
+
   const authenticateFixed = (letters: Itoken[]) => {
-    fetch(`http://localhost:5050/api/users/${userId}/fixed`, {
-      method: 'POST',
-      body: JSON.stringify(convertPayload(letters))
-    }).then(r => r.json()).then(d => console.log(d))
-    setPromptType('flex')
-  }
+    const pythonEndpoint = `http://127.0.0.1:8000/fixed/${userId}`;
+    fetch(pythonEndpoint, {
+      method: "POST",
+      body: JSON.stringify(convertPayload(letters)),
+    })
+      .then((res) => res.json())
+      .then((sucess) => setFixedAuth(sucess));
+    setPromptType("flex");
+  };
 
   const authenticateFlex = (letters: Itoken[]) => {
-    fetch(`http://localhost:5050/api/users/${userId}/flex`, {
-      method: 'POST',
-      body: JSON.stringify(convertPayload(letters))
-    }).then(r => r.json()).then(d => console.log(d))
-    setPromptType('done')
-  }
+    const pythonEndpoint = `http://127.0.0.1:8000/flex/${userId}`;
+    fetch(pythonEndpoint, {
+      method: "POST",
+      body: JSON.stringify(convertPayload(letters)),
+    })
+      .then((res) => res.json())
+      .then((sucess) => setFlexAuth(sucess));
+    setPromptType("done");
+  };
   return (
     <main className={styles.main}>
-      {promptType === 'fixed' ? (
+      {promptType === "fixed" ? (
         <OnlineController
           classNames={styles.main}
           apiUrl={`http://localhost:5050/api/users/${userId}/prompt`}
           onFinished={authenticateFixed}
         />
-      ) : promptType === 'flex' ? (
+      ) : promptType === "flex" ? (
         <OnlineController
           classNames={styles.main}
           apiUrl={`http://localhost:5050/api/prompt/random`}
           onFinished={authenticateFlex}
         />
       ) : (
-        <>gotowe</>
+        <div className={styles["result-text"]}>
+          <p>Authentication with fixed: {fixedAuth ? "true" : "false"}</p>
+          <p>Authentication with flex: {flexAuth ? "true" : "false"}</p>
+        </div>
       )}
     </main>
+  );
+};
 
-  )
-}
-
-const ProfileList = ({selectUserId}: {selectUserId: (userId: number) => void}) => {
-  const [ profiles, setProfiles ] = useState([])
+const ProfileList = ({
+  selectUserId,
+}: {
+  selectUserId: (userId: number) => void;
+}) => {
+  const [profiles, setProfiles] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:5050/api/users')
-      .then(response => response.json())
-      .then(data => {
-        setProfiles(data.map((user: any) => ({
-          username: user.userName,
-          id: user.id,
-        })));
-      })
-  }, [])
+    fetch("http://localhost:5050/api/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setProfiles(
+          data.map((user: any) => ({
+            username: user.userName,
+            id: user.id,
+          }))
+        );
+      });
+  }, []);
   return (
     <div>
       Authenticate as:
       <ul>
-        {profiles.map(({username, id}) => (
+        {profiles.map(({ username, id }) => (
           <li onClick={() => selectUserId(id)}>{username}</li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 const WordState = () => {
-  const [ selectedUserId, setSelectedUserId ] = useState<number>();
+  const [selectedUserId, setSelectedUserId] = useState<number>();
 
-  if(!selectedUserId) {
-    return (
-      <ProfileList selectUserId={(userId) => setSelectedUserId(userId)} />
-    )
+  if (!selectedUserId) {
+    return <ProfileList selectUserId={(userId) => setSelectedUserId(userId)} />;
   }
-  return (
-    <Auth userId={selectedUserId}/>
-  )
-}
+  return <Auth userId={selectedUserId} />;
+};
 
 export default WordState;
